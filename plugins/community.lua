7310cf2e0375530c5c6fb67261ed487e4eab70d7
Rei Armenia <5091445+rarmenia@users.noreply.github.com>
2023-07-27 07:35:14 -0400

    update config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,12 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  --
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript"},
+  { import = "astrocommunity.pack.tailwindcss"},
+  { import = "astrocommunity.pack.angular"},
 }
